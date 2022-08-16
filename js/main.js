82e436c9305847f16c9fe309c1d6c94d3aa32382
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-08-16 12:56:41 +0200

    étape 13

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function valideRecherche() {
+    let searchValue = document.getElementById("recherche").value;
+
+    if(searchValue == "") {
+        $("#myModalBadComment").modal('show');
+    }
+    else {
+        $("#myModal").modal('show');
+    }
+}
